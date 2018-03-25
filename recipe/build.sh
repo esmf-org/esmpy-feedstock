90da5dda99167aa0757e4b853756477a03c5bcb9
Filipe Fernandes <ocefpaf@gmail.com>
2018-03-25 14:48:25 -0300

    don't test with the source

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,8 @@
 
 export ESMFMKFILE=${PREFIX}/lib/esmf.mk
 
-ESMPY_SRC=${SRC_DIR}/src/addon/ESMPy
-cd ${ESMPY_SRC}
+cd ${SRC_DIR}/src/addon/ESMPy
 
 
 ${PYTHON} setup.py build --ESMFMKFILE=${ESMFMKFILE}
 ${PYTHON} setup.py install --record record.txt
-${PYTHON} setup.py test || exit 1
-
